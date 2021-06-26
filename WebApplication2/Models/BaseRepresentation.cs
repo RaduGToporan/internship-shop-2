b7fb590553d1e4734e88ff49718357806e636521
RaduGToporan <radutoporan@yahoo.com>
2021-06-26 17:09:23 +0300

    better version

---
+++ WebApplication2/Models/BaseRepresentation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class BaseRepresentation
+    {
+        public string Message { get; set; }
+        public BaseRepresentation()
+        {
+        }
+    }
+}
--- WebApplication2/Models/CategoryRepresentation.cs
+++ WebApplication2/Models/CategoryRepresentation.cs
@@ -1,9 +1,20 @@
 ﻿using Newtonsoft.Json;
+using WebApplication2.Domain;
 
 namespace WebApplication2.Models
 {
-    public class CategoryRepresentation
+    public class CategoryRepresentation : BaseRepresentation
     {
+        public CategoryRepresentation()
+        {
+           
+        }
+        public CategoryRepresentation(Category category)
+        {
+            CategoryID = category.CategoryID;
+            Name = category.Name;
+        }
+
         public CategoryRepresentation(int categoryID, string name)
         {
             this.CategoryID = categoryID;
